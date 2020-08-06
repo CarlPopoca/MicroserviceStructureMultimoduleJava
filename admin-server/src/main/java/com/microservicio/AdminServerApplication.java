645816dcc414269508ad97b41ca1c95fdd0ef1d0
Carlos Popoca <popocas21@hotmail.com>
2020-08-05 21:03:35 -0500

    Actualización

--- admin-server/src/main/java/com/microservicio/AdminServerApplication.java
+++ admin-server/src/main/java/com/microservicio/AdminServerApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+import org.springframework.context.annotation.Configuration;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableAdminServer
+@Configuration
 public class AdminServerApplication {
 
 	public static void main(String[] args) {
